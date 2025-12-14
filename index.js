1c68d060cd870485657c67c1fa76de7d0a59f63e
ShihabSaheer <shihabshaheer8@gmail.com>
2025-12-14 11:06:45 +0530

    index.html updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end("Hello from GitHub Actions + Jenkins + GitHub Actions");
+  res.end("Hello from GitHub Actions + Jenkins + GitHub Actions V1");
 }).listen(3000, () => {
   console.log("App running on port 3000");
 });
